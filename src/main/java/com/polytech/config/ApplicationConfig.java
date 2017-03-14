0cdf22e49e507b78e428f243dc5a15794a076694
sergio <sergiogalan-delea@htomail.fr>
2017-03-14 12:42:54 +0100

    update 14/03/2017 11h18 avec datasource + sql database + recup post bdd utilisation server mysql + fichier properties

--- src/main/java/com/polytech/config/ApplicationConfig.java
+++ src/main/java/com/polytech/config/ApplicationConfig.java
@@ -5,8 +5,10 @@ import com.polytech.business.PublicationServiceImpl;
 import com.polytech.repository.PostRepository;
 import com.polytech.repository.jdbcPostRepository;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
@@ -14,7 +16,30 @@ import javax.sql.DataSource;
  * Created by norabbit on 13/03/17.
  */
 @Configuration
+@PropertySource("classpath:/application.properties")
 public class ApplicationConfig {
+
+    @Value("${datasource.driverName}")
+    private String driverClassName;
+
+    @Value("${datasource.url}")
+    private String url;
+
+    @Value("${datasource.username}")
+    private String username;
+
+    @Value("${datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(driverClassName);
+        return dataSource;
+    }
 
     //@Bean
     //public DataSource dataSource(){
@@ -24,15 +49,15 @@ public class ApplicationConfig {
     //            .build();
     //}
 
-    @Bean
-    public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/bddpolytech");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        return dataSource;
-    }
+    //@Bean
+    //public DataSource dataSource() {
+    //    BasicDataSource dataSource = new BasicDataSource();
+    //    dataSource.setUsername("root");
+    //    dataSource.setPassword("root");
+    //    dataSource.setUrl("jdbc:mysql://localhost:3306/bddpolytech");
+    //    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+    //    return dataSource;
+    //}
 
     @Bean
     public PostRepository postRepository(){
